d8e6eb02116bbcb2f359dc151d31c85dae159320
Emilio Ormeño <eormneno@gmail.com>
2024-11-06 12:27:40 -0300

    changes

--- clients/android/code_input_dialog.gd
+++ clients/android/code_input_dialog.gd
@@ -3,16 +3,6 @@ extends Control
 # Paths to index scene
 const INDEX_SCENE_PATH = "res://index.tscn"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_button_pressed() -> void:
 	var código = $"%LineEdit".text
 	if código == "123456":
