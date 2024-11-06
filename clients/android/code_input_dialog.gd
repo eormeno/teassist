1f789af0330afa0930d9173276362d32c53e0da9
Emilio Ormeño <eormneno@gmail.com>
2024-11-06 12:26:04 -0300

    changes

--- clients/android/code_input_dialog.gd
+++ clients/android/code_input_dialog.gd
@@ -1,5 +1,7 @@
 extends Control
 
+# Paths to index scene
+const INDEX_SCENE_PATH = "res://index.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	var código = $"%LineEdit".text
+	if código == "123456":
+		save_code(código)
+		get_tree().change_scene_to_file(INDEX_SCENE_PATH)
+	else:
+		$"%ErrorLabel".text = "Còdigo incorrecto!"
+
+# Save the code to local storage
+func save_code(code: String) -> void:
+	var config = ConfigFile.new()
+	config.set_value("code", "value", code)
+	config.save("user://stored_code.cfg")
---
+++ clients/android/index.gd
@@ -0,0 +1,35 @@
+extends Node
+
+# Paths to other scenes
+const MAIN_SCENE_PATH = "res://MainScene.tscn"
+const CODE_INPUT_SCENE_PATH = "res://CodeInputDialog.tscn"
+
+# Stored code value
+var stored_code: String
+
+func _ready():
+	# Check if there is a stored code in local storage
+	stored_code = get_stored_code()
+	
+	if not stored_code:
+		# If there is a stored code, call deferred to load the MainScene
+		# change_scene_to_file is a blocking call, so we call it deferred
+		call_deferred("load_code_input_scene")
+	else:
+		# If no stored code, load the MainScene
+		call_deferred("load_main_scene")
+
+func get_stored_code() -> String:
+	# Implement logic to retrieve code from local storage
+	var config = ConfigFile.new()
+	var err = config.load("user://stored_code.cfg")
+	if err == OK:
+		return config.get_value("code", "value", "")
+	else:
+		return ""
+	
+func load_main_scene():
+	get_tree().change_scene_to_file(MAIN_SCENE_PATH)
+
+func load_code_input_scene():
+	get_tree().change_scene_to_file(CODE_INPUT_SCENE_PATH)
