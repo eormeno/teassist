4e12fa2463958c4c7c1f4f88c780d95bea6428cd
DAMO\Emilio <eormeno@gmail.com>
2024-11-13 19:57:36 -0300

    cambios en consumo de api

--- clients/android/main_scene.gd
+++ clients/android/main_scene.gd
@@ -2,8 +2,26 @@ extends Control
 
 func _ready() -> void:
 	$HTTPRequest.request_completed.connect(_on_request_completed)
-	$HTTPRequest.request("https://api.github.com/repos/godotengine/godot/releases/latest")
+	var code = get_stored_code()
+	$HTTPRequest.request("http://127.0.0.1:8000/api/patient-activities/" + code)
 
 func _on_request_completed(_result, _response_code, _headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json["name"])
+	var paciente = json['patient']
+	var activities = json['activities']
+	$Nombres.text = paciente['nombres']
+	$Apellidos.text = paciente['apellidos']
+	$TextureRect.texture = base64_to_image(activities[0]['activity_thumbnail'])
+
+func get_stored_code() -> String:
+	# Implement logic to retrieve code from local storage
+	var config = ConfigFile.new()
+	var _err = config.load("res://stored_code.cfg")
+	return config.get_value("code", "value", "")
+
+func base64_to_image(base64_string: String) -> Image:
+	print(base64_string)
+	var img = Image.new()
+	#'image' is the resulted base64 string from the API
+	img.load_jpg_from_buffer((base64_string).to_ascii_buffer())
+	return img
