36a40d6578a68cdac4a8c39a013742a10e950d2d
EmanuelPintor2 <emapintor22@gmail.com>
2025-07-24 13:42:05 -0300

    corriendo 7/25

--- src/database/factories/ActivityFactory.php
+++ src/database/factories/ActivityFactory.php
@@ -11,11 +11,18 @@ class ActivityFactory extends Factory
     {
         $images = ImageUtils::downloadImage(256, 256);
 
+        if (!is_array($images) || !isset($images['original'], $images['thumbnail'])) {
+            $images = [
+                'original' => 'default.jpg',
+                'thumbnail' => 'default_thumb.jpg',
+            ];
+        }
+
         return [
             'name' => $this->faker->sentence(2),
             'description' => $this->faker->paragraph(),
             'image' => $images['original'],
-            'thumbnail' => $images['thumbnail']
+            'thumbnail' => $images['thumbnail'],
         ];
     }
 }
