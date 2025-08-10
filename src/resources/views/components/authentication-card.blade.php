bdfd5b06befa64bc0318600d0022f61fc9beddb7
EmanuelPintor2 <emapintor22@gmail.com>
2025-08-10 18:31:20 -0300

    estilos login

--- src/resources/views/components/authentication-card.blade.php
+++ src/resources/views/components/authentication-card.blade.php
@@ -3,7 +3,7 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-indigo-300 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-[#F2EBDC] shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
